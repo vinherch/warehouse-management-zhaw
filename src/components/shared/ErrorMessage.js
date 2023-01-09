1ee4374eb8c300561377c53a46e3d5c7786e65c7
Vincenz Herzog <herzogv88@gmail.com>
2023-01-09 21:57:58 +0100

    Add ErrorMessage handler & LoadingSpinner component

--- src/components/shared/ErrorMessage.js
+++ src/components/shared/ErrorMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ErrorMessage({ error }) {
   return (
-    <div className="error-container">
+    <div className="error-container" style={{ marginTop: "20vh" }}>
       <div>
         <img src="./assets/error.png" alt="" />
         <h3>Ups! Irgendwas ist schief gelaufen!</h3>
--- src/components/shared/LoadingSpinner.js
+++ src/components/shared/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from "react";
 import loadingSpinner from "../../utils/loadingspinner.gif";
 
 function LoadingSpinner() {
-  return <img src={loadingSpinner} alt="Loading..." style={{ width: "200px", margin: "auto", display: "block" }}></img>;
+  return <img src={loadingSpinner} alt="Loading..." style={{ width: "200px", margin: "auto", marginTop: "30vh", display: "block" }}></img>;
 }
 
 export default LoadingSpinner;
